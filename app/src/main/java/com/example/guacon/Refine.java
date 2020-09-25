0e4090a0c594333b8cc2eb521d3288b108432824
jennyli1299 <jennyli1299@users.noreply.github.com>
2020-09-25 13:38:10 -0400

    Refine page

--- app/src/main/java/com/example/guacon/Refine.java
+++ app/src/main/java/com/example/guacon/Refine.java
@@ -2,13 +2,25 @@ package com.example.guacon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Refine extends AppCompatActivity {
+
+    Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refine);
+
+        ok = (Button) findViewById(R.id.btn_ok);
+    }
+
+    public void okay(View v) {
+        Intent mainPgIntent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(mainPgIntent);
     }
 }
