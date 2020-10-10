7109fedce150a142f4da12304033740dd982a71c
GabrielleShang <shang22j@mtholyoke.edu>
2020-10-10 14:05:44 -0400

    Added main search menu

--- app/src/main/java/com/example/guacon/MainActivity.java
+++ app/src/main/java/com/example/guacon/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button refine;
+    Button[] options= new Button[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(refineIntent);
             }
         });
+
+        options[0]=(Button) findViewById(R.id.btn_Breakfast);
+        options[1]=(Button) findViewById(R.id.btn_lunch);
+        options[2]=(Button) findViewById(R.id.btn_dinner);
+        options[3]=(Button) findViewById(R.id.btn_snack);
+        options[4]=(Button) findViewById(R.id.btn_searchAll);
+        //direct the buttons to search results
+        for(int i=0;i<5;i++){
+            options[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent searchIndent = new Intent(getBaseContext(), SearchResult.class);
+                    startActivity(searchIndent);
+                }
+            });
+        }
 
     }
 
