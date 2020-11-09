17ae2ad378efec6d3262225f82f290df67817341
Madison Brading <brading@usc.edu>
2020-11-08 16:25:24 -0800

    notes

--- app/src/main/java/com/example/guacon/Recipe.java
+++ app/src/main/java/com/example/guacon/Recipe.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Recipe extends AppCompatActivity {
 
     //fields
+    TextView instructions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        instructions = (TextView) findViewById(R.id.instructions_txt);
     }
 
     @Override
@@ -20,4 +26,5 @@ public class Recipe extends AppCompatActivity {
         super.onBackPressed();
         startActivity(new Intent(getApplicationContext(), Category_List.class));
     }
+
 }
