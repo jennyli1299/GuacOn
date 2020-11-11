426af7b064919db2bcaed60c309862ea62fd2a9f
Madison Brading <brading@usc.edu>
2020-11-11 08:27:39 -0800

    onCreate ImageView initialization

--- app/src/main/java/com/example/guacon/Recipe.java
+++ app/src/main/java/com/example/guacon/Recipe.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Recipe extends AppCompatActivity {
@@ -17,6 +18,12 @@ public class Recipe extends AppCompatActivity {
     TextView cook_time;
     TextView Ingredients;
 
+    ImageView ic_vegetarian;
+    ImageView ic_gluten_free;
+    ImageView ic_vegan;
+    ImageView ic_dairy_free;
+    ImageView ic_naturally_sweetened;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +32,15 @@ public class Recipe extends AppCompatActivity {
 
         Instructions = (TextView) findViewById(R.id.instructions_txt);
         Recipe = (TextView) findViewById(R.id.recipe_text);
-        Image_PlaceHolder = (TextView) findViewById(R.id.img_place);
         prep_time = (TextView) findViewById(R.id.prep_txt);
         cook_time = (TextView) findViewById(R.id.cook_txt);
         Ingredients = (TextView) findViewById(R.id.ingredients_txt);
+
+        ic_vegetarian = (ImageView) findViewById(R.id.imageView);
+        ic_gluten_free = (ImageView) findViewById(R.id.imageView2);
+        ic_vegan = (ImageView) findViewById(R.id.imageView3);
+        ic_dairy_free = (ImageView) findViewById(R.id.imageView4);
+        ic_naturally_sweetened = (ImageView) findViewById(R.id.imageView5);
 
     }
 
