6b73d4370b77998b002b45d815d6d0705c0a72ca
Ashley S <Vernkost@users.noreply.github.com>
2020-12-26 21:35:51 -0500

    Added constraints and landscape
    Added constraints to recipe form as well as a landscape version of the form. Also coded that when the user hits cancel on the form they will be returned to the profile page

--- app/src/main/java/com/example/guacon/RecipeForm.java
+++ app/src/main/java/com/example/guacon/RecipeForm.java
@@ -2,14 +2,29 @@ package com.example.guacon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 //enter the details for your recipe and add them to our database
 public class RecipeForm extends AppCompatActivity {
+
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_form);
+        cancel = (Button) findViewById(R.id.button2);
+
+        // Goes back to the profile page if the user hits cancel
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Profile.class));
+            }
+        });
     }
+
 }
