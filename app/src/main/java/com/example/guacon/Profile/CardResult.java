58cd79adbf6126d486768f2cd50a8f0109dfae55
ns-github14 <sharmaneelanshi00@gmail.com>
2021-05-20 20:37:04 +0530

    Splash Screen and Shimmer Layout

--- app/src/main/java/com/example/guacon/Profile/CardResult.java
+++ app/src/main/java/com/example/guacon/Profile/CardResult.java
@@ -1,6 +1,5 @@
 package com.example.guacon.Profile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
